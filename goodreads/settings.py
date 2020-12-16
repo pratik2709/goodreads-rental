1c96e4ed006d49450608e75c7fe19c42dcc32339
pratiksaxena <pratik2709@gmail.com>
2020-12-16 18:39:23 +0530

    added new app

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rent_engine.apps.RentEngineConfig',
     'rest_framework',
     'drf_yasg',
     'rest_framework.authtoken',
---
+++ rent_engine/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RentEngineConfig(AppConfig):
+    name = 'rent_engine'

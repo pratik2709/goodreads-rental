7d4999d0bed405f61ca9b457d6ef3f7bba987f1a
pratiksaxena <pratik2709@gmail.com>
2020-12-20 12:20:47 +0530

    removing volumes and adding data through entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+python manage.py loaddata rent_engine/fixtures/data.json
+python manage.py loaddata rent_engine/fixtures/customer.json
+python manage.py loaddata rent_engine/fixtures/version_3.json
+python manage.py load_data
 gunicorn goodreads.wsgi:application --bind 0.0.0.0:8000
 
